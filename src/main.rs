17af03323e8bd192a30c8736ed3c6d57e77825cc
U16A0-U16B9 <xxvirginslayer420xx@gmail.com>
2022-06-01 15:32:36 +0200

    Creation | MVP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use message_data::MessageData;
+use handle_rep::*;
+use teloxide::prelude::*;
+pub mod message_data;
+pub mod handle_rep;
+
+#[tokio::main]
+async fn main() {
+	pretty_env_logger::init();
+	let bot = Bot::from_env().auto_send();
+
+	teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
+
+		let data = MessageData::get_data(&message);
+		let result = HandledReputation::handle_rep(&data);
+		match result {
+			Some(_handled_reputation) => {
+				bot.send_message(
+					data.get_chat_id(),
+					format!(
+						"{} has {} reputation of {} to {}",
+						_handled_reputation.giver_username.unwrap(),
+						_handled_reputation.operation,
+						_handled_reputation.reciv_username.unwrap(),
+            			_handled_reputation.reciv_reputation
+					)
+				).await?;
+			},
+			None => (),
+		}
+
+		respond(())
+	})
+	.await;
 }
---
+++ src/message_data/services.rs
@@ -0,0 +1,149 @@
+use teloxide::types::Message;
+use teloxide::types::User;
+use teloxide::types::UserId;
+use super::triggers::*;
+use super::MessageData;
+
+pub fn get_replied_user_id(message: &Message) -> Option<UserId> {
+	match message.reply_to_message() {
+		Some(_replied) => {
+			match _replied.from() {
+				Some(_user) => {
+					Some(_user.id)
+				},
+				None => None,
+			}
+		}
+		None => {
+			None
+		},
+	}
+}
+
+pub fn get_message_user_id(message: &Message) -> Option<UserId> {
+	match message.from() {
+		Some(_sender) => {
+			Some(_sender.id)
+		},
+		None => {
+			None
+		},
+	}
+}
+
+pub fn get_replied_user_name(message: &Message) -> Option<String> {
+	match message.reply_to_message() {
+		Some(_replied) => {
+			match _replied.from() {
+				Some(_user) => {
+					generate_display_name(_user)
+				},
+				None => todo!(),
+			}
+		}
+		None => {
+			None
+		},
+	}
+}
+
+pub fn get_message_user_name(message: &Message) -> Option<String> {
+	match message.from() {
+		Some(_sender) => {
+			generate_display_name(_sender)
+		},
+		None => {
+			None
+		},
+	}
+}
+
+pub fn get_is_replied_user_bot(message: &Message) -> bool {
+	match message.reply_to_message() {
+		Some(_replied) => {
+			match _replied.from() {
+				Some(_user) => {
+					_user.is_bot
+				},
+				None => false,
+			}
+		}
+		None => {
+			false
+		},
+	}
+}
+
+pub fn get_message_trigger_type(message: &Message) -> TriggerType {
+
+	match message.text() {
+		Some(_text) => {
+			for trigger in POSITIVE_TRIGGERS.iter() {
+				if _text.to_lowercase().contains(trigger) {
+					return TriggerType::Positive;
+				}
+			}
+		
+			for trigger in NEGATIVE_TRIGGERS.iter() {
+				if _text.to_lowercase().contains(trigger) {
+					return TriggerType::Negative;
+				}
+			}
+
+			TriggerType::None
+		},
+		None => {
+			TriggerType::None
+		},
+	}
+}
+
+pub fn get_is_message_trigger(trigger_type: &TriggerType) -> bool {
+	match trigger_type {
+		TriggerType::None => false,
+		TriggerType::Positive => true,
+		TriggerType::Negative => true,
+	}
+}
+
+pub fn calculate_if_data_is_valid(data: &mut MessageData) -> bool {
+	if !data.get_is_trigger() {
+		return false;
+	}
+
+	if data.get_is_rep_reciv_bot() {
+		return false;
+	}
+	let rep_giver_user_id;
+	let rep_reciv_user_id;
+
+	match data.get_rep_giver_user_id() {
+		Some(_user_id) => (rep_giver_user_id = _user_id.clone()),
+		None => return false,
+	}
+
+	match data.get_rep_reciv_user_id() {
+		Some(_user_id) => (rep_reciv_user_id = _user_id.clone()),
+		None => return false,
+	}
+
+	if rep_giver_user_id == rep_reciv_user_id {
+		return false;
+	}
+
+	true
+}
+
+fn generate_display_name(user: &User)  -> Option<String> {
+	let firstname = &user.first_name;
+	let username = &user.username;
+	match username {
+		Some(_username) =>  {
+			Some(_username.to_string())
+		},
+		None => {
+			Some(firstname.to_string())
+		}
+	}
+
+}
